66cfba73231b7a2d68b436a8fe0b15a425071f1e
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-05-30 14:48:50 +0200

    the rest of the PhoneNumber component

--- src/components/PhoneNumber.tsx
+++ src/components/PhoneNumber.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { UserState, SetFormField } from "../App";
-import { validateInput } from "../utils/ValidationRules";
+import FormInput from "./FormInput";
 
 type PhoneNumberProps = {
   user: UserState;
@@ -8,40 +8,23 @@ type PhoneNumberProps = {
 };
 
 function PhoneNumber({ user, setFormField }: PhoneNumberProps) {
-  const [isPhoneNumberTouched, setIsPhoneNumberTouched] = useState(false);
-
-  const phoneNumberError = isPhoneNumberTouched
-    ? validateInput(user.phoneNumber, [
+  return (
+    <FormInput
+      id="phoneNumber"
+      label="Phone Number"
+      placeholderText="Enter your phone number"
+      type="text"
+      value={user.phoneNumber}
+      setFormField={setFormField}
+      validationRules={[
         { type: "required", value: true, message: "Phone number is required" },
         {
           type: "pattern",
-          value: /^[0-9]*$/,
-          message: "Phone number must contain only numbers",
+          value: /^\d{10}$/,
+          message: "Phone number is not valid",
         },
-      ])
-    : "";
-
-  return (
-    <div className="flex flex-col space-y-4">
-      <div className="flex flex-col">
-        <label
-          htmlFor="phoneNumber"
-          className="text-3xl my-10 font-bold text-center"
-        >
-          Phone number
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="border-2 border-gray-300 bg-transparent p-2 rounded-md"
-          value={user.phoneNumber}
-          onChange={(e) => setFormField("phoneNumber", e.target.value)}
-          onBlur={() => setIsPhoneNumberTouched(true)}
-        />
-
-        {phoneNumberError && <p className="text-red-500">{phoneNumberError}</p>}
-      </div>
-    </div>
+      ]}
+    />
   );
 }
 
