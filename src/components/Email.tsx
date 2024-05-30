456e1730a69d7f68bfd7b8c89412dc90820b0c1e
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-05-30 10:27:17 +0200

    refactor name from formState to user

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import { FormState, SetFormField } from "../App";
+import { UserState, SetFormField } from "../App";
 import { validateInput } from "../utils/ValidationRules";
 
 type EmailProps = {
-  formState: FormState;
+  user: UserState;
   setFormField: SetFormField;
 };
 
-function Email({ formState, setFormField }: EmailProps) {
+function Email({ user, setFormField }: EmailProps) {
   const [isEmailTouched, setIsEmailTouched] = useState(false);
 
   const emailError = isEmailTouched
-    ? validateInput(formState.email, [
+    ? validateInput(user.email, [
         { type: "required", value: true, message: "Email is required" },
       ])
     : "";
@@ -26,7 +26,7 @@ function Email({ formState, setFormField }: EmailProps) {
           type="email"
           id="email"
           className="border-2 border-gray-300 bg-transparent p-2 rounded-md"
-          value={formState.email}
+          value={user.email}
           onChange={(e) => setFormField("email", e.target.value)}
           onBlur={() => setIsEmailTouched(true)}
         />
