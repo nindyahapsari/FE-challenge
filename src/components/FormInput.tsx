0046b301d02cecbbc8d1cba597d4655947db995c
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-05-30 15:25:34 +0200

    add check icon if the input value does not raise an error

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { SetFormField } from "../App";
 import { validateInput, ValidationRule } from "../utils/ValidationRules";
+import { CheckIcon } from "@heroicons/react/24/solid";
 
 type FormInputProps = {
   id: string;
@@ -32,15 +33,22 @@ function FormInput({
         <label htmlFor={id} className="text-3xl my-24 font-bold text-center">
           {label}
         </label>
-        <input
-          type={type}
-          id={id}
-          placeholder={placeholderText}
-          className="border-2 border-gray-300 bg-transparent p-2 rounded-md mx-2 focus:border-blue-500 focus:outline-none"
-          value={value}
-          onChange={(e) => setFormField(id, e.target.value)}
-          onBlur={() => setIsTouched(true)}
-        />
+        <div className="flex flex-row justify-center items-center">
+          <input
+            type={type}
+            id={id}
+            placeholder={placeholderText}
+            className="border-2 border-gray-300 bg-transparent p-2 rounded-md mx-2 focus:border-blue-500 focus:outline-none"
+            value={value}
+            onChange={(e) => setFormField(id, e.target.value)}
+            onBlur={() => setIsTouched(true)}
+          />
+          <CheckIcon
+            className={`h-5 w-5 ${
+              !error && isTouched ? "text-green-400" : "text-gray-500"
+            }`}
+          />
+        </div>
         {error && <p className="text-red-500 m-2">{error}</p>}
       </div>
     </div>
